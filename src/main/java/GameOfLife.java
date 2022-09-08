ad56a8426d484970af2b367b70264a83965e8c0a
tiagobica <tiago.bica@codurance.com>
2022-09-08 11:22:45 +0100

    3 Cells cases

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,6 +1,6 @@
 public class GameOfLife {
 
-    private boolean[][] board;
+    private final boolean[][] board;
 
     public GameOfLife(boolean[][] board) {
         this.board = board;
@@ -10,8 +10,29 @@ public class GameOfLife {
     }
 
     public void nextGen() {
-        if(board[0][0]){
+        if (board[0].length > 2 &&
+                board[0][0] && board[0][1] && !board[0][2]) {
             board[0][0] = false;
+            board[0][1] = false;
+            return;
+        }
+        if (board[0].length > 2 &&
+            board[0][0] && board[0][1] && board[0][2]) {
+            board[0][0] = false;
+            board[0][2] = false;
+            return;
+        }
+        if (board[0].length > 0 && board[0][0]) {
+            board[0][0] = false;
+            return;
+        }
+        if (board[0].length > 1 && board[0][1]) {
+            board[0][1] = false;
+            return;
+        }
+        if (board[0].length > 2 && board[0][2]) {
+            board[0][2] = false;
+            return;
         }
     }
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -1,6 +1,7 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
 
@@ -8,30 +9,83 @@ class GameOfLifeTest {
     public void should_return_initial_grid() {
         boolean [][] board = {{true}};
         GameOfLife gameOfLife = new GameOfLife(board);
-        Assertions.assertArrayEquals(new boolean[][] {{true}}, gameOfLife.getState());
+
+        boolean[][] expectedGrid = {{true}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
     }
 
     @Test
     public void should_return_initial_grid_false() {
         boolean [][] board = {{false}};
         GameOfLife gameOfLife = new GameOfLife(board);
-        Assertions.assertArrayEquals(new boolean[][] {{false}}, gameOfLife.getState());
+
+        boolean[][] expectedGrid = {{false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
     }
 
     @Test
     public void live_cell_should_return_false_1_neighbor() {
         boolean [][] board = {{true}};
         GameOfLife gameOfLife = new GameOfLife(board);
+
         gameOfLife.nextGen();
-        Assertions.assertArrayEquals(new boolean[][] {{false}}, gameOfLife.getState());
+
+        assertArrayEquals(new boolean[][] {{false}}, gameOfLife.getState());
     }
 
     @Test
     public void when_lonely_a_dead_cell_remains_dead_cell() {
         boolean [][] board = {{false}};
         GameOfLife gameOfLife = new GameOfLife(board);
+
         gameOfLife.nextGen();
-        Assertions.assertArrayEquals(new boolean[][] {{false}}, gameOfLife.getState());
+
+        boolean[][] expectedGrid = {{false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
+    }
+
+    @Test
+    public void with_2_cells_one_alive_will_die() {
+        boolean [][] board = {{false,true}};
+        GameOfLife gameOfLife = new GameOfLife(board);
+
+        gameOfLife.nextGen();
+
+        boolean[][] expectedGrid = {{false, false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
+    }
+
+    @Test
+    public void with_3_cells_one_alive_will_die() {
+        boolean [][] board = {{false,false,true}};
+        GameOfLife gameOfLife = new GameOfLife(board);
+
+        gameOfLife.nextGen();
+
+        boolean[][] expectedGrid = {{false, false, false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
+    }
+
+    @Test
+    public void with_3_cells_alive_middle_cell_will_live() {
+        boolean [][] board = {{true,true,true}};
+        GameOfLife gameOfLife = new GameOfLife(board);
+
+        gameOfLife.nextGen();
+
+        boolean[][] expectedGrid = {{false,true,false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
+    }
+
+    @Test
+    public void with_3_cells_2_neighbours_alive_then_all_die() {
+        boolean [][] board = {{true,true,false}};
+        GameOfLife gameOfLife = new GameOfLife(board);
+
+        gameOfLife.nextGen();
+
+        boolean[][] expectedGrid = {{false,false,false}};
+        assertArrayEquals(expectedGrid, gameOfLife.getState());
     }
 
 }
